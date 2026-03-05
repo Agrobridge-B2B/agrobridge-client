20cf3d50d59cfa1a1a0d70c9a3d99ff156cae0a4
Mohammed Latrach <m.latrach.youcode@gmail.com>
2026-03-05 20:20:58 +0000

    AG-43 Creating the components for the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/home/HeroSection';
-import GlobalMarketplace from '@/components/home/GlobalMarketplace';
-import PlatformFeatures from '@/components/home/PlatformFeatures';
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/home/HeroSection";
+import GlobalMarketplace from "@/components/home/GlobalMarketplace";
+import PlatformFeatures from "@/components/home/PlatformFeatures";
+import WhyTrustUs from "@/components/home/WhyTrustUs";
+import TeamSection from "@/components/home/TeamSection";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-brand-light">
-      <Navbar />
-      <HeroSection />
-      <GlobalMarketplace />
-      <PlatformFeatures />
-    </div>
-  );
+	return (
+		<div className="min-h-screen bg-brand-light">
+			<Navbar />
+			<HeroSection />
+			<GlobalMarketplace />
+			<PlatformFeatures />
+			<WhyTrustUs />
+			<TeamSection />
+		</div>
+	);
 }
